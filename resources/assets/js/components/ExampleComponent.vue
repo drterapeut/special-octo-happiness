cd0eccb5512c8137aca2e4266de2bcfe93f3b50e
vbalagovic <vedran.balagovic@gmail.com>
2018-02-09 17:36:13 +0100

    Component changed

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="container" style="background-color: white;">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card card-default">
